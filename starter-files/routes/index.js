d0bb77fdd5805a97889d846922de71f82b8a34d9
ChloeJ <chloe.jandsten@gmail.com>
2020-04-24 22:41:03 -0400

    FEATURE: Password reset almost complete

--- starter-files/routes/index.js
+++ starter-files/routes/index.js
@@ -36,6 +36,7 @@ router.post('/register',
 router.get('/logout', authController.logout)
 router.get('/account', authController.isLoggedIn, userController.account)
 router.post('/account', userController.updateAccount)
-
+router.post('/account/forgot', catchErrors(authController.forgot))
+router.get('/account/reset/:token', catchErrors(authController.reset))
 
 module.exports = router;
